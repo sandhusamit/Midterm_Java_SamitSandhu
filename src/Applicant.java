0e4e810c048f2e77191f5dc7a00ccb992cbc7e5d
Samit Sandhu <ssand139@my.centennialcollege.ca>
2025-10-15 10:15:24 -0400

    Initialize git

---
+++ src/Applicant.java
@@ -0,0 +1,36 @@
+
+public abstract class Applicant {
+	/*
+	 * Applicant class skeleton (abstract) for getting earnings of either a hourly employee or 
+	 * a commission employee. 
+	 */
+	
+	String firstName; 
+	String lastName; 
+	long sin; //must be 9 digits max and all integer numbers
+	
+	//constructor for validation and subclass object creations
+	
+    public Applicant(String firstName, String lastName, long sin) {
+    	
+    	//validate values - cannot be empty or null
+        if (firstName == null || firstName.isEmpty() ||
+            lastName == null || lastName.isEmpty())  
+        { 
+    
+            throw new IllegalArgumentException("Fields cannot be empty.");
+        }
+        //sin validation - 9 digits
+        else if(String.valueOf(sin).length() != 9) { 
+        	throw new IllegalArgumentException("SIN must be 9 digits!");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sin = sin;
+    }
+	//will be overridden 
+	public abstract double Earnings();
+	
+	
+
+}
